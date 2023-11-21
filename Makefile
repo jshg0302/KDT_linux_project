10942dac6979d978f2a6d90b2cd39b87b4def328
jshg <j3322814@gmail.com>
2023-11-21 19:33:15 +0900

    day2

--- Makefile
+++ Makefile
@@ -4,27 +4,31 @@ SYSTEM = ./system
 UI = ./ui
 WEB_SERVER = ./web_server
 
+INCLUDES = -I$(SYSTEM) -I$(UI) -I$(WEB_SERVER)
+
 CC = gcc
 
 objects = main.o system_server.o web_server.o input.o gui.o
 
+.PHONY: clean
+
 $(TARGET): $(objects)
-	$(CC) -o $(TARGET) $(objects)
+	$(CC) -o $(TARGET) $(objects) -lpthread
 
 main.o:  main.c
-	$(CC) $@ -c main.c
+	$(CC) -g $(INCLUDES) -c main.c
 
 system_server.o: $(SYSTEM)/system_server.h $(SYSTEM)/system_server.c
-	$(CC) $(SYSTEM)/$@ -c $(SYSTEM)/system_server.c
+	$(CC) -g $(INCLUDES) -c ./system/system_server.c
 
 gui.o: $(UI)/gui.h $(UI)/gui.c
-	$(CC) $(UI)/$@ -c $(UI)/gui.c
+	$(CC) -g $(INCLUDES) -c $(UI)/gui.c
 
 input.o: $(UI)/input.h $(UI)/input.c
-	$(CC) $(UI)/$@ -c $(UI)/input.c
+	$(CC) -g $(INCLUDES) -c $(UI)/input.c
 
 web_server.o: $(WEB_SERVER)/web_server.h $(WEB_SERVER)/web_server.c
-	$(CC) $(WEB_SERVER)/$@ -c $(WEB_SERVER)/web_server.c
+	$(CC) -g $(INCLUDES) -c $(WEB_SERVER)/web_server.c
 
 clean:
 	rm -rf *.o
--- system/system_server.c
+++ system/system_server.c
@@ -1,15 +1,118 @@
+#include <assert.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <sys/prctl.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <time.h>
 
 #include <system_server.h>
 #include <gui.h>
 #include <input.h>
 #include <web_server.h>
 
+static int toy_timer = 0;
+
+static void timer_expire_signal_handler()
+{
+    toy_timer++;
+}
+
+void set_periodic_timer(long sec_delay, long usec_delay)
+{
+	struct itimerval itimer_val = {
+		 .it_interval = { .tv_sec = sec_delay, .tv_usec = usec_delay },
+		 .it_value = { .tv_sec = sec_delay, .tv_usec = usec_delay }
+    };
+
+	setitimer(ITIMER_REAL, &itimer_val, (struct itimerval*)0);
+}
+
+int posix_sleep_ms(unsigned int timeout_ms)
+{
+    struct timespec sleep_time;
+
+    sleep_time.tv_sec = timeout_ms / MILLISEC_PER_SECOND;
+    sleep_time.tv_nsec = (timeout_ms % MILLISEC_PER_SECOND) * (NANOSEC_PER_USEC * USEC_PER_MILLISEC);
+
+    return nanosleep(&sleep_time, NULL);
+}
+
+void *watchdog_thread(void* arg)
+{
+    char *s = arg;
+
+    printf("%s", s);
+
+    while (1) {
+        posix_sleep_ms(5000);
+    }
+
+    return 0;
+}
+
+void *monitor_thread(void* arg)
+{
+    char *s = arg;
+
+    printf("%s", s);
+
+    while (1) {
+        posix_sleep_ms(5000);
+    }
+
+    return 0;
+}
+
+void *disk_service_thread(void* arg)
+{
+    char *s = arg;
+
+    printf("%s", s);
+
+    while (1) {
+        posix_sleep_ms(5000);
+    }
+
+    return 0;
+}
+
+void *camera_service_thread(void* arg)
+{
+    char *s = arg;
+
+    printf("%s", s);
+
+    while (1) {
+        posix_sleep_ms(5000);
+    }
+
+    return 0;
+}
+
 int system_server()
 {
+    struct itimerspec ts;
+    struct sigaction  sa;
+    struct sigevent   sev;
+    timer_t *tidlist;
+    int retcode;
+    pthread_t watchdog_thread_tid, monitor_thread_tid, disk_service_thread_tid, camera_service_thread_tid;
+
     printf("나 system_server 프로세스!\n");
 
+    /* 5초 타이머를 만들어 봅시다. */
+    signal(SIGALRM, timer_expire_signal_handler);
+    /* 5초 타이머 등록 */
+    set_periodic_timer(5, 0);
+
+    /* watchdog, monitor, disk_service, camera_service 스레드를 생성한다. */
+    pthread_create(&watchdog_thread_tid, NULL, watchdog_thread, "watchdog_thread\n");
+    pthread_create(&monitor_thread_tid, NULL, monitor_thread, "monitor_thread\n");
+    pthread_create(&disk_service_thread_tid, NULL, disk_service_thread, "disk_service_thread thread\n");
+    pthread_create(&camera_service_thread_tid, NULL, camera_service_thread, "camera_service_thread\n");
+
+    printf("system init done.  waiting...");
     while (1) {
         sleep(1);
     }
@@ -23,6 +126,20 @@ int create_system_server()
     const char *name = "system_server";
 
     printf("여기서 시스템 프로세스를 생성합니다.\n");
+
+    /* fork 를 이용하세요 */
+    switch (systemPid = fork()) {
+    case -1:
+        printf("fork failed\n");
+    case 0:
+        /* 프로세스 이름 변경 */
+        if (prctl(PR_SET_NAME, (unsigned long) name) < 0)
+            perror("prctl()");
+        system_server();
+        break;
+    default:
+        break;
+    }
 
     return 0;
 }
