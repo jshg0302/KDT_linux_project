c1ee1219025694ffc4920317a38bfa07d493940c
jshg <j3322814@gmail.com>
2023-11-23 15:45:02 +0900

    day4-file_system_call

--- system/system_server.c
+++ system/system_server.c
@@ -25,6 +25,7 @@
 #include <camera_HAL.h>
 #include <toy_message.h>
 #include <shared_memory.h>
+// #include <dump_state.h>
 
 #define BUF_LEN 1024
 #define TOY_TEST_FS "./fs"
@@ -126,6 +127,7 @@ void *watchdog_thread(void* arg)
 }
 
 #define SENSOR_DATA 1
+#define DUMP_STATE 2
 
 void *monitor_thread(void* arg)
 {
@@ -150,10 +152,47 @@ void *monitor_thread(void* arg)
             printf("sensor info: %d\n", the_sensor_info->press);
             printf("sensor humidity: %d\n", the_sensor_info->humidity);
             toy_shm_detach(the_sensor_info);
+        } else if (msg.msg_type == DUMP_STATE) {
+            // 여기에 dumpstate를 구현해 주세요.
+            // dumpstate();
+        } else {
+            printf("monitor_thread: unknown message. xxx\n");
         }
     }
 
     return 0;
+}
+
+// https://stackoverflow.com/questions/21618260/how-to-get-total-size-of-subdirectories-in-c
+static long get_directory_size(char *dirname)
+{
+    DIR *dir = opendir(dirname);
+    if (dir == 0)
+        return 0;
+
+    struct dirent *dit;
+    struct stat st;
+    long size = 0;
+    long total_size = 0;
+    char filePath[1024];
+
+    while ((dit = readdir(dir)) != NULL) {
+        if ( (strcmp(dit->d_name, ".") == 0) || (strcmp(dit->d_name, "..") == 0) )
+            continue;
+
+        sprintf(filePath, "%s/%s", dirname, dit->d_name);
+        if (lstat(filePath, &st) != 0)
+            continue;
+        size = st.st_size;
+
+        if (S_ISDIR(st.st_mode)) {
+            long dir_size = get_directory_size(filePath) + size;
+            total_size += dir_size;
+        } else {
+            total_size += size;
+        }
+    }
+    return total_size;
 }
 
 
@@ -170,9 +209,30 @@ void *disk_service_thread(void* arg)
 
     printf("%s", s);
 
-    // 여기에 구현
-    while (1) {
-        sleep(1);
+    inotifyFd = inotify_init();                 /* Create inotify instance */
+    if (inotifyFd == -1)
+        return 0;
+
+    wd = inotify_add_watch(inotifyFd, TOY_TEST_FS, IN_CREATE);
+    if (wd == -1)
+        return 0;
+
+    for (;;) {                                  /* Read events forever */
+        numRead = read(inotifyFd, buf, BUF_LEN);
+        if (numRead == 0) {
+            printf("read() from inotify fd returned 0!");
+            return 0;
+        }
+
+        if (numRead == -1)
+            return 0;
+
+        for (p = buf; p < buf + numRead; ) {
+            event = (struct inotify_event *) p;
+            p += sizeof(struct inotify_event) + event->len;
+        }
+        total_size = get_directory_size(TOY_TEST_FS);
+        printf("directory size: %d\n", total_size);
     }
 
     return 0;
@@ -199,6 +259,10 @@ void *camera_service_thread(void* arg)
         printf("msg.param2: %d\n", msg.param2);
         if (msg.msg_type == CAMERA_TAKE_PICTURE) {
             toy_camera_take_picture();
+        } else if (msg.msg_type == DUMP_STATE) {
+            toy_camera_dump();
+        } else {
+            printf("camera_service_thread: unknown message. xxx\n");
         }
     }
 
